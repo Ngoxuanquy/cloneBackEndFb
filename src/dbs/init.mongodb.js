b61e0afd447ee9221b4467edc07b5de517464f54
Ngoxuanquy <ngoxuanquy1812@gmail.com>
2023-08-07 13:43:30 +0700

    quy

--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -1,47 +1,3 @@
-// const mongoose = require('mongoose')
-
-// mongoose.set('strictQuery', true)
-
-// const {
-//     db: { host, port, name },
-// } = require('../configs/config.mongodb')
-
-// const connectString = `mongodb://${host}:${port}/${name}`
-
-// console.log('connectString::', connectString)
-
-// const { countConnect } = require('../helpers/check.connect')
-
-// class Database {
-//     constructor() {
-//         this.connect()
-//     }
-
-//     connect(type = 'mongodb') {
-//         if (1 === 1) {
-//             mongoose.set('debug', true)
-//             mongoose.set('debug', { color: true })
-//         }
-//         mongoose
-//             .connect(connectString)
-//             .then((_) => {
-//                 console.log('Connected success', countConnect())
-//             })
-//             .catch((err) => console.log(`Error Connect`))
-//     }
-
-//     static getInstance() {
-//         if (!Database.instance) {
-//             Database.instance = new Database()
-//         }
-
-//         return Database.instance
-//     }
-// }
-
-// const instanceMongodb = Database.getInstance()
-
-// module.exports = instanceMongodb
 
 
 const mongoose = require('mongoose')
@@ -56,6 +12,7 @@ mongoose.set('strictQuery', true)
 // const connectString = `mongodb://${host}:${port}/${name}`
 const connectString = 'mongodb+srv://quyquy:QyHalVDd06UVkrt1@cluster0.waezzqe.mongodb.net/?retryWrites=true&w=majority'
 // w1GzFcrU99975hDQ
+
 
 
 const { countConnect } = require('../helpers/check.connect')
